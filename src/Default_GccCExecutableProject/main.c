9c9d3eb8c951b8740bf18322540fbf428ba197f5
Ferdinand Lukasak <feroalien@hotmail.com>
2018-01-30 07:36:08 +1000

    Added default project for GCC c executable project

---
+++ src/Default_GccCExecutableProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * Default_GccCExecutableProject.c
+ *
+ * Created: 30/01/2018 7:33:45 AM
+ * Author : Ferdinand Lukasak
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
