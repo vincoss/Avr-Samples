ad949271ae473ed6f30dd7f56655371aa7393888
Ferdinand Lukasak <feroalien@hotmail.com>
2018-01-29 21:52:15 +1000

    Added sample project with make file

---
+++ src/MakeFileSample/Utility.c
@@ -0,0 +1,14 @@
+/*
+*  main.c
+*
+*	Created: 27/01/2018 9:43:48 PM
+*	Author : Ferdinand Lukasak
+*/
+
+
+#include "Utility.h"
+
+int Utility_GetSamplePin(void)
+{
+	return 5;
+}
---
+++ src/MakeFileSample/main.c
@@ -0,0 +1,39 @@
+/*
+*  main.c
+*
+*	Created: 27/01/2018 9:43:48 PM
+*	Author : Ferdinand Lukasak
+
+	Copy main.hex into Arduino board use this command
+	1. Open command prompt
+	2. cd {SolutionDir}\debug
+	3. avrdude -p atmega328p -c arduino -P COM3 -b 115200 -D -U flash:w:main.hex
+
+*/
+
+#ifndef F_CPU
+# define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "Utility.h"
+
+
+int main(int argc, char *argv[])
+{
+	int ledPinSampleOne = Utility_GetSamplePin();
+
+	DDRB |= (1 << ledPinSampleOne); // pinMode(LED, OUTPUT); // sets the gitital pin as putput
+
+	while (1)
+	{
+		PORTB |= (1 << ledPinSampleOne); // digitalWrite(LED, ON); // turns the LED on
+		_delay_ms(1000);
+
+		PORTB &= ~(1 << ledPinSampleOne); // digitalWrite(LED, OFF); // turns the LED off
+		_delay_ms(500);
+	}
+
+	return 0;
+}
