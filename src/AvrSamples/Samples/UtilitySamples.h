5d173ec74ea9d13a373a109030c356e59b42b483
Ferdinand Lukasak <feroalien@hotmail.com>
2018-04-16 08:02:26 +1000

    samples update

--- src/AvrSamples/Samples/UtilitySamples.h
+++ src/AvrSamples/Samples/UtilitySamples.h
@@ -14,7 +14,7 @@
 
 void UtilitySamples_To_uint8_t_FromString(void)
 {
-	char buffer[8];
+	char buffer[12];
 	uint8_t result = ConvertToUInt8("255");
 	char * str = IntToString(result, "%hhu", buffer, sizeof(buffer));
 	
@@ -22,10 +22,20 @@ void UtilitySamples_To_uint8_t_FromString(void)
 	UsartWriteChar('\n');
 }
 
+void UtilitySamples_To_int32_t_FromString(void)
+{
+	char buffer[12];
+	unsigned long long int result = ConvertToInt32("2147483647");
+	char * str = IntToString(result, "%ld", buffer, sizeof(buffer));
+	
+	UsartWriteCharString(str);
+	UsartWriteChar('\n');
+}
+
 void UtilitySamples_To_uint32_t_FromString(void)
 {
-	char buffer[8];
-	unsigned long long int result = ConvertToUnsignedInt64("?4294967295?");
+	char buffer[12];
+	unsigned long long int result = ConvertToUnsignedInt64("4294967295?");
 	char * str = UnsignedIntToString(result, "%lu", buffer, sizeof(buffer));
 	
 	UsartWriteCharString(str);
@@ -34,7 +44,7 @@ void UtilitySamples_To_uint32_t_FromString(void)
 
 void UtilitySamples_To_Float_FromString(void)
 {
-	char buffer[8];
+	char buffer[12];
 	float result = ConvertToFloat("-3.1415");
 	char * str = FloatToString(result, "%f", buffer, sizeof(buffer));
 	
@@ -44,7 +54,7 @@ void UtilitySamples_To_Float_FromString(void)
 
 void UtilitySamples_To_Double_FromString(void)
 {
-	char buffer[8];
+	char buffer[12];
 	double result = ConvertToDouble("-3.1416");
 	char * str = FloatToString(result, "%lf", buffer, sizeof(buffer));
 	
