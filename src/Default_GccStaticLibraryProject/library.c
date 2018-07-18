227d813d96450a1abf66e9b5c7d74a1519f04a27
Ferdinand Lukasak <feroalien@hotmail.com>
2018-07-18 21:28:42 +1000

    added default example projects

---
+++ src/Default_GccStaticLibraryProject/library.c
@@ -0,0 +1,16 @@
+/*
+ * Default_GccStaticLibraryProject.c
+ *
+ * Created: 18/07/2018 9:28:04 PM
+ * Author : Ferdinand
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
