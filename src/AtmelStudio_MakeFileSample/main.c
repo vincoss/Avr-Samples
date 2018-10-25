098ee8630102ac1d8d4a6c823002d42eb4790bc6
Ferdinand Lukasak <feroalien@hotmail.com>
2018-10-26 07:39:35 +1000

    include directory fix

--- src/AtmelStudio_MakeFileSample/main.c
+++ src/AtmelStudio_MakeFileSample/main.c
@@ -7,7 +7,7 @@
 	Copy main.hex into Arduino board use this command
 	1. Open command prompt
 	2. cd {SolutionDir}\debug
-	3. avrdude -p atmega328p -c arduino -P COM3 -b 115200 -D -U flash:w:main.hex
+	3. avrdude -p atmega328p -c arduino -P COM6 -b 57600 -D -U flash:w:main.hex
 
 */
 
